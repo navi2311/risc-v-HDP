90cfa314e3f1b8a620cf026540321e676af8500b
navi2311 <134842758+navi2311@users.noreply.github.com>
2024-03-06 21:47:29 -0800

    Update alu.c

--- week1/part_b/alu.c
+++ week1/part_b/alu.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, result;
+    char operation[10];
+
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+    printf("Enter the required operation: ");
+    scanf("%s", operation);
+
+    if (strcmp(operation, "add") == 0) {
+        result = a + b;
+        printf("Value of %d + %d is %d\n", a, b, result);
+    } else if (strcmp(operation, "sub") == 0) {
+        result = a - b;
+        printf("Value of %d %s %d is %d\n", a, operation, b, result);
+    } else if (strcmp(operation, "mul") == 0) {
+        result = a * b;
+        printf("Value of %d %s %d is %d\n", a, operation, b, result);
+    } else if (strcmp(operation, "div") == 0) {
+        if (b == 0) {
+            printf("Can't divide when b is zero\n");
+        } else {
+            result = a / b;
+            printf("Value of %d %s %d is %d\n", a, operation, b, result);
+        }
+    } else {
+        result = 0;
+        printf("No operation is selected or invalid syntax\n");
+    }
+
+    return 0;
+}
 
